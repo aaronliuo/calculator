cdcfa80cad4f7980d562fa950ab66ad2ea0cb30f
Aaron Liu <aaron.j.liu@gmail.com>
2022-05-27 17:53:22 -0400

    Added working multiplication Added working division with zero error Added working addition Added working subtraction Added working equals button Added capability to perform operations onto previous computed answer

--- operations.js
+++ operations.js
@@ -1,39 +1,81 @@
 
 function add(a, b) {
-    return a+b;
+    display.textContent = a+b;
 }
 
 function substract(a, b) {
-    return a-b;
+    display.textContent = a-b;
 }
 
 function multiple(a, b) {
-    return a*b;
+    display.textContent =  a*b;
 }
 function divide(a, b) {
-    return a/b;
+    if(b == 0) display.textContent = -1;
+    else display.textContent = a/b;
 }
 
-function operate(operator, a, b) {
-    if(operator == '+') {
-        return add(a, b);
+function operate() {
+    const a = parseInt(initialNumber);
+    const b = parseInt(secondNumber);
+    if(operatorValue == '+') {
+        add(a, b);
     }
-    else if(operator == '-') {
-        return substract(a, b);
+    else if(operatorValue == '-') {
+        substract(a, b);
     }
-    else if(operator == '*') {
-        return multiple(a, b);
+    else if(operatorValue == '*') {
+        multiple(a, b);
     }
     else {
-        if(b == 0) return "NO";
-        return divide(a, b);
+        divide(a, b);
+    }
+    const equals = document.querySelector('.equal');
+    equals.removeEventListener('click', operate);
+    operatorPressed = false;
+    secondNumber = "";
+    initialNumber = display.textContent;
+}
+
+function joinNumber(num) {
+    const value = num.target.textContent;
+    if(!operatorPressed) {
+        initialNumber += value;
+        display.textContent = initialNumber;
+    }
+    else {
+        secondNumber += value;
+        display.textContent = secondNumber;
     }
 }
 
+function setOperator(operator) {
+    if(operatorPressed == true || initialNumber == "") return;
+    operatorValue = operator.target.textContent;
+    operatorPressed = true;
+
+    const equals = document.querySelector('.equal');
+    equals.addEventListener('click', operate);
+
+}
+
+function setEventListeners() {
+    const numbers = document.querySelectorAll('.number');
+    numbers.forEach((num) => {
+        num.addEventListener('click', joinNumber);
+    })
+
+    const operations = document.querySelectorAll('.operation');
+    operations.forEach((operation) => {
+        operation.addEventListener('click', setOperator);
+    })
+
+}
+
+const display = document.querySelector('.display');
 let initialNumber = "";
 let secondNumber = "";
+let operatorValue = "";
+let operatorPressed = false;
 
-console.log(operate('+', 1, 2));
-console.log(operate('-', 1, 2));
-console.log(operate('*', 3, 2));
-console.log(operate('/', 1, 2));
+setEventListeners();
